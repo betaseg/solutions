29141b754bd71092c095e07d3ce795dd9dc8742c
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2023-06-23 12:46:40 +0200

    add csbdeep predict solution, adapt dtype conversion for stardist solution

--- src/io.github.betaseg/stardist_train/solution.py
+++ src/io.github.betaseg/stardist_train/solution.py
@@ -66,7 +66,8 @@ def run():
         X = tuple(imread(str(f)) for f in tqdm(fx))
         Y = tuple(fill_label_holes(imread(str(f))) for f in tqdm(fy))
 
-        X = tuple(_X.astype(np.uint32) for _X in X)
+        if any(_X.dtype == np.intc or _X.dtype == np.uintc for _X in X):
+            X = tuple(_X.astype(np.uint32) for _X in X)
         Y = tuple(_Y.astype(np.uint8) for _Y in Y)
 
         if normalize_img:
