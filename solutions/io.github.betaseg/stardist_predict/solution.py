33afdd5a245722ecc778afc507cb7d66a63bad91
Scaramir <maxotto45@gmail.com>
2023-12-28 20:35:00 +0100

    Adding new/updated io.github.betaseg_stardist_predict_0.1.0

--- solutions/io.github.betaseg/stardist_predict/solution.py
+++ solutions/io.github.betaseg/stardist_predict/solution.py
@@ -1,5 +1,6 @@
 from album.runner.api import get_args, setup, get_environment_path
 import sys
+
 env_file = """name:  stardist_train
 channels:
   - conda-forge
@@ -34,48 +35,53 @@ dependencies:
     - gputools
 """
 
+
 def run():
-    from csbdeep.utils.tf import limit_gpu_memory
-    limit_gpu_memory(fraction=0.8, total_memory=12000)
     from csbdeep.utils import Path
     from tifffile import imread, imsave
     import numpy as np
     from stardist.models import StarDist3D
 
+    def predict_tif(f, model, outdir, x0):
+        # compute n_tiles
+        n_tiles = tuple(int(np.ceil(s / 160)) for s in x0.shape)
+        print(f"using {n_tiles} tiles")
+
+        # normalize
+        print("normalizing...")
+        x = x0.astype(np.float32) / 255
+        # predict
+        y, polys = model.predict_instances(x, n_tiles=n_tiles)
+        rays = polys["rays"]
+        polys["rays_vertices"] = rays.vertices
+        polys["rays_faces"] = rays.faces
+        # save output
+        out = Path(outdir)
+        out.mkdir(exist_ok=True, parents=True)
+        imsave(out / f"{Path(f).stem}.stardist.tif", y)
+        np.savez(out / f"{Path(f).stem}.stardist.npz", **polys)
+
     # parse arguments
     args = get_args()
 
     # options
-    fname = args.fname_input
     model_name = args.model_name
     outdir = args.output_dir
     basedir = args.model_basedir
 
-    # load file
-    x0 = imread(fname)
-
-    # compute n_tiles
-    n_tiles = tuple(int(np.ceil(s / 160)) for s in x0.shape)
-    print(f"using {n_tiles} tiles")
-
     # load model and apply it to the stack
     model = StarDist3D(None, name=model_name, basedir=basedir)
 
-    # normalize
-    print("normalizing...")
-    x = x0.astype(np.float32) / 255
-
-    # predict
-    y, polys = model.predict_instances(x, n_tiles=n_tiles)
-    rays = polys["rays"]
-    polys["rays_vertices"] = rays.vertices
-    polys["rays_faces"] = rays.faces
-
-    # save output
-    out = Path(outdir)
-    out.mkdir(exist_ok=True, parents=True)
-    imsave(out / f"{Path(fname).stem}.stardist.tif", y)
-    np.savez(out / f"{Path(fname).stem}.stardist.npz", **polys)
+    # load files
+    input_path = Path(args.fname_input)
+    if input_path.is_dir():
+        for f in input_path.iterdir():
+            if f.suffix == ".tif":
+                x0 = imread(f)
+                predict_tif(f, model, outdir, x0)
+    else:
+        x0 = imread(input_path)
+        predict_tif(input_path, model, outdir, x0)
 
 
 setup(
@@ -103,17 +109,17 @@ setup(
     args=[
         {
             "name": "fname_input",
-            "description": "Path to the input file.",
+            "description": "Path of the input tif file or folder of tif images.",
             "required": True
         },
         {
             "name": "model_name",
-            "description": "Path to the input model.",
+            "description": "Name of the folder in which the input model is stored.",
             "required": True
         },
         {
             "name": "model_basedir",
-            "description": "Path to the model directory.",
+            "description": "Path to the directory that contains the folders (model_name) for each individual model.",
             "required": True
         },
         {
