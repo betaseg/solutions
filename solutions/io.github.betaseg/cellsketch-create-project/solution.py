1625781430384fa97f4ba0252ad3fb34ffdab74e
Scaramir <maxotto45@gmail.com>
2023-12-28 20:33:07 +0100

    Adding new/updated io.github.betaseg_cellsketch-create-project_0.2.1

--- solutions/io.github.betaseg/cellsketch-create-project/solution.py
+++ solutions/io.github.betaseg/cellsketch-create-project/solution.py
@@ -66,10 +66,10 @@ setup(
     description="This solution creates a new CellSketch project by importing the raw dataset as well as masks and labelings of cell components.",
     tags=["bdv", "cellsketch", "segmentation", "annotation"],
     cite=[{
-        "text": "A. Müller, D. Schmidt, C. S. Xu, S. Pang, J. V. D’Costa, S. Kretschmar, C. Münster, T. Kurth, F. Jug, M. Weigert, H. F. Hess, M. Solimena; 3D FIB-SEM reconstruction of microtubule–organelle interaction in whole primary mouse β cells. J Cell Biol 1 February 2021; 220 (2): e202010039.",
+        "text": "A. Müller, D. Schmidt, C. S. Xu, S. Pang, J. V. D'Costa, S. Kretschmar, C. Münster, T. Kurth, F. Jug, M. Weigert, H. F. Hess, M. Solimena; 3D FIB-SEM reconstruction of microtubule-organelle interaction in whole primary mouse β cells. J Cell Biol 1 February 2021; 220 (2): e202010039.",
         "doi": "https://doi.org/10.1083/jcb.202010039"
     }, {
-        "text": "Pietzsch, T., Saalfeld, S., Preibisch, S., & Tomancak, P. (2015). BigDataViewer: visualization and processing for large image data sets. Nature Methods, 12(6), 481–483.",
+        "text": "Pietzsch, T., Saalfeld, S., Preibisch, S., & Tomancak, P. (2015). BigDataViewer: visualization and processing for large image data sets. Nature Methods, 12(6), 481-483.",
         "doi": "10.1038/nmeth.3392"
     }],
     album_api_version="0.5.5",
